0a7ff6ba98e9d70ac16e6791882bc051e3c94853
Shardul1719 <shardulj26@gmail.com>
2025-08-07 12:46:52 +0530

    Updated vite config and package.json for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Xora-landing/'
+  base: '/saas_landing_page/'
 });
