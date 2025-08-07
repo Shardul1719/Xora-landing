876a847e356cc2101371463902a5fcfdef81e583
Shardul1719 <shardulj26@gmail.com>
2025-08-07 11:53:29 +0530

    Local changes before pull

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Xora-landing/', // 👈 this line is essential for GitHub Pages
   plugins: [react()],
 })
