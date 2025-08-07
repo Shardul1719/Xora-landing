8bded15913eee38b647f1ad81cfa1b82d4c6456f
Shardul1719 <shardulj26@gmail.com>
2025-08-07 12:05:28 +0530

    Cleaned up and reset project to base state

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Xora-landing/', // 👈 this line is essential for GitHub Pages
   plugins: [react()],
 })
